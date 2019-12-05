d8e5cc1bd758d2f3b9569de4ca3072a3e003aab4
aniqmir <aniqmir@gmail.com>
2019-12-05 18:02:07 +0500

    signExtension css adjusted

--- src/containers/colorComponents/index.js
+++ src/containers/colorComponents/index.js
@@ -1,16 +1,15 @@
-import React from "react";
-import Home from "./home/home.jsx";
-import SeeExistingAccounts from "./seeExistingAccounts/seeExistingAccounts.jsx";
+import React from 'react';
+import Home from './home/home.jsx';
+import SeeExistingAccounts from './seeExistingAccounts/seeExistingAccounts.jsx';
 import SignExtension from './signExtension/signExtension.jsx';
 
-
-import { latestSignReq } from '../../pages/Background/index'
+import { latestSignReq } from '../../pages/Background/index';
 //import SignExtension from './signExtension/signExtension.jsx';
 export default function Index(props) {
   function allStorage() {
     var values = [],
       keys = Object.keys(localStorage).map((type, key) => {
-        if (type.includes("cosmos-wallets-colors")) {
+        if (type.includes('cosmos-wallets-colors')) {
           return type;
         } else {
           return undefined;
@@ -31,17 +30,18 @@ export default function Index(props) {
 
   // }, [])
   const usersExist = allStorage();
-  console.log(latestSignReq, 'popup')
+  console.log(latestSignReq);
   return (
-    <div>
-      {
-        latestSignReq.length === 0 ? usersExist.length !== 0 ? (
+    <React.Fragment>
+      {latestSignReq === undefined ? (
+        usersExist.length !== 0 ? (
           <SeeExistingAccounts logo={props.logo} />
         ) : (
-            <Home logo={props.logo} />
-          )
-          : <SignExtension />
-      }
-    </div>
+          <Home logo={props.logo} />
+        )
+      ) : (
+        <SignExtension />
+      )}
+    </React.Fragment>
   );
 }
--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -4,7 +4,7 @@ import '../../assets/img/icon-128.png';
 import { storeWallet } from '@rnssolution/color-keys';
 import { Queue } from './queue';
 const q = new Queue();
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   if (request.method == 'getStatus') {
     if (sender.url === 'http://localhost:3000/') {
       sendResponse({ status: localStorage });
@@ -13,8 +13,7 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     } else {
       sendResponse({ status: 'No Data for you Bro' });
     }
-  }
-  else if (request.method == 'setextensionaddress') {
+  } else if (request.method == 'setextensionaddress') {
     try {
       storeWallet(
         request.data.wallet,
@@ -29,7 +28,8 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     var values = allStorage();
     sendResponse({ values });
   } else if (request.method == 'LUNIE_SIGN_REQUEST') {
-    console.log("LUNIE_SIGN_REQUEST_Saad")
+    console.log('LUNIE_SIGN_REQUEST_Saad');
+    // q.add()
     //chrome.runtime.sendMessage('LUNIE_SIGN_REQUEST_Saad')
     sendResponse({ status: 'success', type: 'LUNIE_SIGN_REQUEST_Saad' });
   } else sendResponse({}); // snub them.
@@ -55,9 +55,9 @@ function allStorage() {
   return values;
 }
 
-
 // q.add(1);
 // q.add(2);
 // q.add(3);
-// export var latestSignReq = q.size !== 0 ? q.first() : '';
+console.log('size', q.size());
+export var latestSignReq = q.size !== 0 ? q.first() : '';
 // console.log('backgrounD', latestSignReq);
