e9f282906e209fdaa445dac11cb5a591e2d17139
saadrns <saad@rnssol.com>
2019-12-13 16:40:15 +0500

    Remove Logs

--- src/pages/Content/modules/print.js
+++ src/pages/Content/modules/print.js
@@ -2,16 +2,8 @@ var temp = 0;
 export const printLine = (line, arg2) => {
   console.log('===> FROM THE PRINT MODULE:', line, arg2);
 };
-chrome.runtime.onMessage.addListener(function(request, sender) {
-  console.log('request.la', request);
-  //console.log("request.signature", request.signature.toString('base64'))
-  // if (request.signature && request.publicKey)
-  //   dataextension.push({
-  //     signature: request.signature,
-  //     publicKey: request.publicKey,
-  //   });
+chrome.runtime.onMessage.addListener(function (request, sender) {
   if (request.method == 'rejectsignaccount') {
-    console.log('reject in print js');
     var data = {
       message: {
         payload: {
@@ -22,7 +14,6 @@ chrome.runtime.onMessage.addListener(function(request, sender) {
       type: 'FROM_LUNIE_EXTENSION',
     };
   } else {
-    console.log('else reject in bg');
     var data = {
       message: {
         payload: {
@@ -39,33 +30,15 @@ chrome.runtime.onMessage.addListener(function(request, sender) {
   console.log('Done Signing');
 });
 
-// export const getDatafromExtension = (data) => {
-//   // console.log('data from extension', data);
-// };
 
 function getData() {
-  chrome.runtime.sendMessage({ method: 'getextensionaddress' }, function(
+  chrome.runtime.sendMessage({ method: 'getextensionaddress' }, function (
     response
   ) {
     return JSON.stringify(response.values[0]);
   });
 }
 
-// function listenerFunc() {
-//   chrome.runtime.onMessage.addListener(
-//     function (request, sender, sendResponse) {
-//     //  console.log("ExtensionListener")
-//       // console.log(sender.tab ?
-//       //   "from a content script:" + sender.tab.url :
-//       //   "from the extension");
-//       if (request.greeting == "hello")
-//         sendResponse({ farewell: "goodbye" });
-//     });
-// }
-//chrome.runtime.onMessage.addListener(function (data, n, t) {
-
-//var r = formatting(data.type, data.payload); window.postMessage(r, "*")
-//})
 function INIT_EXTENSION() {
   //var e = o("INIT_EXTENSION", { extension_enabled: !0 });
   var e = {
@@ -75,8 +48,7 @@ function INIT_EXTENSION() {
   window.postMessage(e, '*');
 }
 
-window.addEventListener('message', function(e) {
-  //console.log("temp", temp % 5, temp)
+window.addEventListener('message', function (e) {
   if (temp == 0) {
     temp = temp + 1;
     INIT_EXTENSION();
@@ -92,10 +64,9 @@ window.addEventListener('message', function(e) {
   ) {
     //listenerFunc()
     // var dataextension;
-    chrome.runtime.sendMessage({ method: 'getextensionaddress' }, function(
+    chrome.runtime.sendMessage({ method: 'getextensionaddress' }, function (
       response
     ) {
-      // dataextension = JSON.stringify(response.values)//JSON.parse(JSON.stringify(response.values[0]));
       var dataextension = [];
 
       if (response.values) {
@@ -103,10 +74,6 @@ window.addEventListener('message', function(e) {
           if (type.address && type.name)
             dataextension.push({ address: type.address, name: type.name });
         });
-
-        // if (obj && obj.name && obj.address) {
-        //   dataextension.push({ "address": obj.name, "name": obj.address });
-        // }
       }
 
       var data = {
@@ -127,11 +94,9 @@ window.addEventListener('message', function(e) {
     e.data.payload &&
     e.data.payload.type == 'LUNIE_SIGN_REQUEST'
   ) {
-    console.log('123');
     chrome.runtime.sendMessage(
       { method: 'LUNIE_SIGN_REQUEST', data: e.data },
-      function(response) {
-        console.log('[][][][][][][][][]', response);
+      function (response) {
         var data = [
           {
             payload: {
