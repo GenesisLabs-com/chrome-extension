0105bed396aa2e4bea246a05ed0abaf22d56ec81
saadrns <saad@rnssol.com>
2019-12-13 12:53:46 +0500

    delegate

---
+++ src/containers/colorComponents/Delegate/delegate.jsx
@@ -0,0 +1,20 @@
+<div><div data-v-0a68b168="" class="session-approve"><h2 data-v-0a68b168="">Approve Transaction</h2> <br data-v-0a68b168=""> <div data-v-0a68b168="" class="from">
+    From
+    <div data-v-0a68b168="" class="bech32-address"><div class="address">
+        colors…4xlm
+  </div> <div class="copied"><i class="material-icons">check</i></div></div></div> <div data-v-0a68b168="" class="tm-form-group"><!----> <!----> <div class="tm-form-group__field"><div data-v-0a68b168="" class="tx"><div class="tx__icon"><img src="/images/color.svg" alt="cosmic atom token" class="staking"></div> <div data-v-bc107f32="" class="tx__content"><div data-v-bc107f32="" session-address="colors18mdxmk4cqdna47hu68g5snsz8zgrw357pn4xlm" class="tx__content__left"><div class="tx__content__caption"><p>
+        Delegated
+      <b>1</b> <span>&nbsp;CLR</span></p></div> <div class="tx__content__information">
+            To&nbsp;
+    <a href="#/staking/validators/colorsvaloper1q6scldk68xpg4jez6a257ezxacv9alvy6g8jpt" class="">
+                colorsvaloper1q6scldk68xpg4jez6a257ezxacv9alvy6g8jpt
+    </a></div></div> <!----></div></div> <div data-v-7030068d="" data-v-0a68b168="" class="approval-table"><ul data-v-7030068d="" class="table-invoice"><li data-v-7030068d=""><span data-v-7030068d="">Subtotal</span> <span data-v-7030068d=""> 1.000000 CLR </span></li> <li data-v-7030068d=""><span data-v-7030068d="">Network Fee</span> <span data-v-7030068d="">
+            0.000109
+            CLR
+      </span></li> <li data-v-7030068d="" class="total-row"><span data-v-7030068d="">Total</span> <span data-v-7030068d=""> 1.000108 CLR </span></li></ul></div> <div data-v-0a68b168="" class="action-modal-group tm-form-group"><!----> <label for="password" class="tm-form-group__label">
+            Password
+  </label> <div class="tm-form-group__field"><input data-v-0a68b168="" type="password" placeholder="Password" class="tm-field" id="password"> <!----> <!----></div></div> <div data-v-0a68b168="" class="session-approve-footer"><button data-v-21b3c012="" data-v-0a68b168="" class="button left-button secondary" id="reject-btn">
+                Reject
+</button> <button data-v-21b3c012="" data-v-0a68b168="" class="button right-button" id="approve-btn">
+                    Approve
+</button></div></div></div></div></div>
