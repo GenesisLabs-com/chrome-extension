ce457d08ba6ecf5fca52aa28aedadfc5d8ae3daf
saadrns <saad@rnssol.com>
2019-12-13 12:36:52 +0500

    Proposal

---
+++ src/containers/colorComponents/proposal/proposal.jsx
@@ -0,0 +1,15 @@
+<div><div data-v-0a68b168="" class="session-approve"><h2 data-v-0a68b168="">Approve Transaction</h2> <br data-v-0a68b168=""> <div data-v-0a68b168="" class="from">
+    From
+    <div data-v-0a68b168="" class="bech32-address"><div class="address">
+        colors…4xlm
+  </div> <div class="copied"><i class="material-icons">check</i></div></div></div> <div data-v-0a68b168="" class="tm-form-group"><!----> <!----> <div class="tm-form-group__field"><div data-v-0a68b168="" class="tx"><div class="tx__icon"><img src="/images/color.svg" alt="cosmic atom token" class="governance"></div> <div data-v-bc107f32="" class="tx__content"><div data-v-bc107f32="" session-address="colors18mdxmk4cqdna47hu68g5snsz8zgrw357pn4xlm" class="tx__content__left"><div class="tx__content__caption"><p>
+        Submitted text proposal
+      <b>10,000</b> <span>CLR</span></p></div> <div class="tx__content__information">
+            Title:
+    <i>Test Proposal</i></div></div> <!----></div></div> <!----> <div data-v-0a68b168="" class="action-modal-group tm-form-group"><!----> <label for="password" class="tm-form-group__label">
+            Password
+  </label> <div class="tm-form-group__field"><input data-v-0a68b168="" type="password" placeholder="Password" class="tm-field" id="password"> <!----> <!----></div></div> <div data-v-0a68b168="" class="session-approve-footer"><button data-v-21b3c012="" data-v-0a68b168="" class="button left-button secondary" id="reject-btn">
+                Reject
+</button> <button data-v-21b3c012="" data-v-0a68b168="" class="button right-button" id="approve-btn">
+                    Approve
+</button></div></div></div></div></div>
